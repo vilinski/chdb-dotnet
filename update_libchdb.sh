e662bda1579d715805a52b7edfbcd591aaddaec0
Andreas Vilinski <vilinski@yahoo.de>
2024-02-20 18:26:59 +0100

    rid packages

--- update_libchdb.sh
+++ update_libchdb.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+# Get the newest release version
+LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/chdb-io/chdb/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+if [ -z "$LATEST_RELEASE" ]; then
+    echo "Latest release not found"
+    exit 1
+fi
+RELEASE=${1:-$LATEST_RELEASE}
+
 # Download the correct version based on the platform
 case "$(uname -s)" in
     Linux)
         if [[ $(uname -m) == "aarch64" ]]; then
             PLATFORM="linux-aarch64"
+            RID="linux-arm64"
         else
             PLATFORM="linux-x86_64"
+            RID="linux-x64"
         fi
         ;;
     Darwin)
         if [[ $(uname -m) == "arm64" ]]; then
             PLATFORM="macos-arm64"
+            RID="osx-arm64"
         else
             PLATFORM="macos-x86_64"
+            RID="osx-x64"
         fi
         ;;
     *)
@@ -21,14 +33,11 @@ case "$(uname -s)" in
         exit 1
         ;;
 esac
+ARCH=${2:-$PLATFORM}
 
-# Get the newest release version
-LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/chdb-io/chdb/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-RELEASE=${1:-$LATEST_RELEASE}
+DOWNLOAD_URL="https://github.com/chdb-io/chdb/releases/download/$RELEASE/$ARCH-libchdb.tar.gz"
 
-DOWNLOAD_URL="https://github.com/chdb-io/chdb/releases/download/$RELEASE/$PLATFORM-libchdb.tar.gz"
-
-echo "Downloading $PLATFORM-libchdb.tar.gz from $DOWNLOAD_URL (latest is $LATEST_RELEASE)"
+echo "Downloading $ARCH-libchdb.tar.gz from $DOWNLOAD_URL (latest is $LATEST_RELEASE)"
 
 # Download the file
 curl -L -o libchdb.tar.gz "$DOWNLOAD_URL"
@@ -41,3 +50,7 @@ chmod +x libchdb.so
 
 # Clean up
 rm -f libchdb.tar.gz
+
+# Move the libchdb.so to the correct runtime folder
+mkdir -p "runtimes/$RID/native"
+mv libchdb.so "runtimes/$RID/native/libchdb.so"
